76b692a722aa64c4e8e1986a9a92308b6b38dd11
shabana1579 <yasmeen.shabana1579@gmail.com>
2022-12-11 00:29:55 +0530

    first commit

---
+++ index1.js
@@ -0,0 +1,11 @@
+function loadCoupon(){
+    document.getElementById('coupon').style.visibility='visible';
+
+document.getElementById('Main').style.opacity='0.5';
+}
+
+function closeCoupon(){
+    document.getElementById('coupon').style.visibility='hidden';
+
+document.getElementById('Main').style.opacity='1';
+}
